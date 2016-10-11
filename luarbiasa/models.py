ed1eed0c25fd09f5634ec27572063091acccb61b
dio <abrahamdio@gmail.com>
2016-10-10 22:05:07 -0500

    prettyfy deals object on admin page

--- luarbiasa/models.py
+++ luarbiasa/models.py
@@ -28,11 +28,6 @@ class constants():
 		('PONDOK_INDAH_MALL_3','Pondok Indah Mall 3'),	
 	)
 
-
-	
-
-
-
 class Deal(models.Model):
 	id = models.AutoField(primary_key=True)		#indexing is enabled automatically for primary key
 	retailer_id = models.ForeignKey('Retailer',blank=True) #indexing is enabled automatically for foreign key
@@ -43,6 +38,8 @@ class Deal(models.Model):
 	summary = models.CharField(max_length=256,blank=True)
 	description = models.CharField(max_length=2000,blank=True)
 	picture_link = models.CharField(max_length=512,blank=True)
+	def __str__(self):
+		return '%s: %s' % (self.retailer_id.brand_name, self.summary)
 
 class Retailer(models.Model):
 	id = models.AutoField(primary_key=True)					  #indexing is enabled automatically for primary key 
