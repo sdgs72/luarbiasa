c47e39bac428ef01ecf976e16b0b4370ea33644e
dio <abrahamdio@gmail.com>
2016-10-11 02:13:54 -0500

    autosuggestion done, mall list is ready, templates and asset management ready

--- luarbiasa/views.py
+++ luarbiasa/views.py
@@ -13,14 +13,13 @@ def index(request):
 	for location in constants.LOCATION_CHOICES:
 		location_choices[location[1]] = location[0]
 	context = {"location_choices_obj": constants.LOCATION_CHOICES, "location_choices_str": json.dumps(location_choices)}
-	return render(request,'index.html',context);
+	return render(request,'luarbiasa/index.html',context);
 
 def getDealsByLocation(request):
 	context = {}
 	myArr = []
 	print request.GET['location']
    	location = request.GET['location']
-	# location = "MALL_KELAPA_GADING"
 	retailer_list = Retailer.objects.filter(location=location).values()
 	q1 = Q()
 	for elem_retailer in retailer_list:
