3f6583503f3e0cc376775d668d240f64c49d8f6e
Dion Hiananto <sdgs72@hotmail.com>
2016-10-13 21:02:04 -0500

    update settings

--- luarbiasa/settings.py
+++ luarbiasa/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT,'assets'),
+    os.path.join(BASE_DIR,'assets'),
 ]
 
 # Simplified static file serving.
